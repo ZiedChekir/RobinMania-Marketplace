6cd41c4147371bd78012e6dbefde2de93feaab9d
Sami <kammounsami77@gmail.com>
2022-04-19 22:49:23 +0100

    Game Items interface changes

--- Marketplace/chain/Interfaces/IGameItems.sol
+++ Marketplace/chain/Interfaces/IGameItems.sol
@@ -6,20 +6,12 @@ interface IGameItems {
         external
         view
         returns (uint256);
-    
     function setApprovalForAll(address _operator, bool _approved) external;
     function safeTransferFrom(
         address _from,
         address _to,
         uint256 _id,
         uint256 _value,
-        bytes calldata _data
-    ) external;
-    function safeBatchTransferFrom(
-        address _from,
-        address _to,
-        uint256[] calldata _ids,
-        uint256[] calldata _values,
         bytes calldata _data
     ) external;
     function isApprovedForAll(address account, address operator) external view returns(bool);
