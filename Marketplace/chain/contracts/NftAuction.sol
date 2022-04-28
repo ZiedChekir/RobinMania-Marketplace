69dba9b2eb30d0be91cfa31cb4d52485c3c1d1b2
zied <ziedchekir1@gmail.com>
2022-04-28 13:20:43 +0100

    Auction implemented with test
    bid history still not implemented

---
+++ Marketplace/chain/contracts/NftAuction.sol
@@ -0,0 +1,156 @@
+pragma solidity ^0.8.12;
+
+import "../Interfaces/IGameItems.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+contract NftAuction  is ERC1155Holder{
+    mapping(uint256 => Auction[]) public AuctionOf;
+    address public admin;
+
+    constructor() {
+
+        admin = msg.sender;
+    }
+    enum AuctionState {
+        OPEN,
+        ENDED,
+        CANCELLED
+    }
+struct Bid {
+        address bidder;
+        uint ammount;
+        uint bidTime;
+    }
+    struct Auction {
+        uint256 minBidIncrement;
+        uint256 auctionStartedAt;
+        //uint32 auctionBidPeriod;
+        uint256 auctionEnd; // we can extend bidding time when a user has made a bid and we only change this value
+        uint256 startPrice;
+        uint256 highestBid;
+        address highestBidder;
+        address seller;
+        AuctionState state;
+        uint256 index;
+        // Bid[] bids; 
+    }
+
+    event NftAuctionCreated(
+        
+        uint256 tokenId,
+        address nftSeller,
+        uint256 minPrice,
+        uint256 auctionBidPeriod,
+        uint256 bidIncreasePercentage
+     
+    );
+    event BidMade(
+        uint256 tokenID,
+        address bidder,
+        uint256 ammount
+    );
+    event AuctionEnded(uint tokenId,uint  index);
+
+
+
+    modifier priceGreaterThanZero(uint x){
+        require(x>0,"price have to be greater than zero");
+        _;
+    }
+
+
+
+
+    function createNewAuctionItem(
+        IGameItems nft,
+        uint256 _tokenId,
+        uint128 _startPrice,
+        uint32 _auctionBidPeriod,
+        uint32 _minBidIncrement
+    ) external
+    priceGreaterThanZero(_startPrice) 
+    {
+        require(nft.balanceOf(msg.sender,_tokenId)>0," you need to own this NFT to auction it ");
+        require(
+            nft.isApprovedForAll(msg.sender, address(this)),
+            "NFT not approved for Marketplace"
+            );
+
+
+     
+
+         Auction memory newAuction = Auction({
+        minBidIncrement:_minBidIncrement,
+        auctionStartedAt:block.timestamp,
+        auctionEnd:block.timestamp + _auctionBidPeriod,
+        startPrice:_startPrice,
+        seller:msg.sender,
+        highestBid:_startPrice,
+        highestBidder:msg.sender,
+        state:AuctionState.OPEN,
+        index:AuctionOf[_tokenId].length
+        // bids:bids
+        });
+
+        nft.safeTransferFrom(newAuction.seller, address(this), _tokenId, 1, new bytes(0));
+        AuctionOf[_tokenId].push(newAuction);
+        emit NftAuctionCreated(_tokenId, msg.sender, _startPrice, _auctionBidPeriod,_minBidIncrement);
+    }
+    function placeBid(uint _tokenId,uint index) public payable priceGreaterThanZero(msg.value) {
+        
+        require(AuctionOf[_tokenId][index].highestBid+AuctionOf[_tokenId][index].minBidIncrement <=msg.value,"bid price is less than the minimum required bid increment");
+        require(AuctionOf[_tokenId][index].state == AuctionState.OPEN);
+        require(AuctionOf[_tokenId][index].seller != msg.sender,"cant bid on own auction");
+        Auction memory auction = AuctionOf[_tokenId][index];
+         //Get data of previous bidder 
+        address prevHighestBidder = auction.highestBidder;
+        uint prevHighestBid = auction.highestBid;
+        // refund the previous bid to the previous highest bidder
+        if(auction.highestBidder!=auction.seller){
+        payable(prevHighestBidder).transfer(prevHighestBid);
+        //address(prevHighestBidder).transfer(prevHighestBid); 
+        }
+        //update the new highest bidder
+        AuctionOf[_tokenId][index].highestBidder = msg.sender;
+        AuctionOf[_tokenId][index].highestBid = msg.value;
+        
+       // Bid memory bid = Bid({bidder:msg.sender,ammount:msg.value,bidTime:block.timestamp});
+         
+      //  AuctionOf[_tokenId][index].bids.push(bid);
+
+     emit BidMade( _tokenId,
+         msg.sender,
+         msg.value);
+    }
+
+
+    function AuctionEnd(IGameItems nft, uint _tokenId, uint index)external payable{
+        require(msg.sender == AuctionOf[_tokenId][index].seller || msg.sender ==AuctionOf[_tokenId][index].highestBidder, "you need to be the owner or the highest bidder");
+        require(getAuctionState(_tokenId,index)==AuctionState.ENDED,"the auction is still open");
+        require(AuctionOf[_tokenId][index].state == AuctionState.OPEN); //it means this is the first time the auction is claimed
+        require(
+            nft.isApprovedForAll(msg.sender, address(this)),
+            "NFT not approved for Marketplace"
+            );
+        require(nft.balanceOf(address(this),_tokenId) >0,"Errorrrita");
+
+
+        nft.safeTransferFrom(address(this),AuctionOf[_tokenId][index].highestBidder,_tokenId,1,new bytes(0));
+        payable(AuctionOf[_tokenId][index].seller).transfer(AuctionOf[_tokenId][index].highestBid);
+        AuctionOf[_tokenId][index].state = AuctionState.ENDED;
+        emit AuctionEnded(_tokenId,index);
+
+    }
+
+    //cancel auction not implemented yet
+    function getAuctionState(uint _tokenId, uint index) public view returns (AuctionState){
+        if(block.timestamp > AuctionOf[_tokenId][index].auctionEnd) return AuctionState.ENDED;
+        else return AuctionState.OPEN;
+    }
+
+    function getAuction(uint _tokenId,uint index)public view returns(Auction memory) {
+        return AuctionOf[_tokenId][index];
+    }
+
+
+}
