34cca1024fb076e8516aeb28b1c373db03f27c09
NoorMbarek <noormbarek1@gmail.com>
2022-05-29 00:04:44 +0100

    Changed name

--- Marketplace/frontend/pages/_app.js
+++ Marketplace/frontend/pages/_app.js
@@ -30,7 +30,7 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>ZNS Marketplace</title>
+        <title>RobinMania Marketplace</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
